fe7b57a9384ca0ddd0efdceb9c65894664b04b6e
Ollivdh <olivier.vandenheede.mailbox@gmail.com>
2019-08-21 13:54:39 +0200

    Updates

---
+++ precache-manifest.7ce964c9a2158b05af4752b94f2bad42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbb3ff2a01acf648d7e371fe9360df3",
+    "url": "/React-E-commerce-store/index.html"
+  },
+  {
+    "revision": "18e55b51c4f429ffef5b",
+    "url": "/React-E-commerce-store/static/css/main.7c5a4460.chunk.css"
+  },
+  {
+    "revision": "0fa69c48ff6c32e81d46",
+    "url": "/React-E-commerce-store/static/js/2.8697d001.chunk.js"
+  },
+  {
+    "revision": "18e55b51c4f429ffef5b",
+    "url": "/React-E-commerce-store/static/js/main.91dc8807.chunk.js"
+  },
+  {
+    "revision": "742a43f4a30e61fbc61e",
+    "url": "/React-E-commerce-store/static/js/runtime~main.e5fb13f3.js"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/React-E-commerce-store/static/media/11.2 shopping-bag.svg.09e6f5e7.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/React-E-commerce-store/static/media/4.4 crown.svg.ed28accb.svg"
+  }
+]);
